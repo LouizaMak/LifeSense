3969c84512603acae84582ae53a65b44eef4778b
Louiza Mak <louiza.mak@gmail.com>
2024-08-29 15:58:19 -0500

    style profile componenet, edit form, and add cursor on button hover

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,8 @@
 import { AppContext } from "./AppContext"
 import { useContext, useState } from "react";
 import ProfileForm from "./ProfileForm";
+import profilePicture from "../images/default_picture.jpg";
+import style from "./profileStyle.css"
 
 function Profile() {
     const { currentUser } = useContext(AppContext)
@@ -12,15 +14,23 @@ function Profile() {
 
     return(
         <>
-            <div>
-                <h1>[Pic Here]</h1>
-                <button onClick={handleToggleForm}>Edit</button>
-                <h1>{currentUser.username}</h1>
-                <p>{currentUser.first_name} {currentUser.last_name}</p>
-                <h2>Joined: {currentUser.date_joined.slice(0,10)}</h2>
-                <p>Age: {currentUser.age}</p>
-                <p>Gender: {currentUser.gender}</p>
-                <button>Settings</button>
+            <div className="profile-block">
+                <div className="profile-intro">
+                    <div className="image-button">
+                        <img src={profilePicture} alt="default profile picture"/>
+                        <button onClick={handleToggleForm}>Edit</button>
+                    </div>
+                    <div>
+                        <h1>{currentUser.username}</h1>
+                        <p>{currentUser.first_name} {currentUser.last_name}</p>
+                    </div>
+                </div>
+                <div className="information-section">
+                    <h2>Joined: {currentUser.date_joined.slice(0,10)}</h2>
+                    <p>Age: {currentUser.age}</p>
+                    <p>Gender: {currentUser.gender}</p>
+                    <button>Settings</button>
+                </div>
             </div>
             {isEditing ? <ProfileForm onToggleForm={handleToggleForm} /> : ""}
         </>
--- client/src/components/ProfileForm.js
+++ client/src/components/ProfileForm.js
@@ -29,31 +29,39 @@ function ProfileForm({ onToggleForm }) {
     })
 
     return (
-        <div className="form-popup" id="profileForm">
-            <h2>Edit Profile</h2>
-            <form className="form-container" onSubmit={handleSubmit}>
-                <input type="text" placeholder="First Name" name="first_name" value={values.first_name} onChange={handleChange} className={errors.first_name && touched.first_name ? 'input-error' : null} required/>
-                {errors.first_name && touched.first_name && (<span className="error">{errors.first_name}</span>)}
+        <>
+            <div className="form-popup" id="profileForm">
+                <form className="edit-form" onSubmit={handleSubmit}>
+                    <h1>Edit Profile</h1>
+                    <div className="edit-text-fields">
+                        <label for="first_name">First Name</label>
+                        <input type="text" placeholder="First Name" name="first_name" value={values.first_name} onChange={handleChange} className={errors.first_name && touched.first_name ? 'input-error' : null} required/>
+                        {errors.first_name && touched.first_name && (<span className="error">{errors.first_name}</span>)}
 
-                <input type="text" placeholder="Last Name" name="last_name" value={values.last_name} onChange={handleChange} className={errors.last_name && touched.last_name_name ? 'input-error' : null} required/>
-                {errors.last_name && touched.last_name && (<span className="error">{errors.last_name}</span>)}
-            
-                <input type="radio" id="f" name="gender" value="F" onChange={handleChange} checked={values.gender === 'F'} required/>
-                <label for="f">Female</label>
+                        <label for="last_name">Last Name</label>
+                        <input type="text" placeholder="Last Name" name="last_name" value={values.last_name} onChange={handleChange} className={errors.last_name && touched.last_name_name ? 'input-error' : null} required/>
+                        {errors.last_name && touched.last_name && (<span className="error">{errors.last_name}</span>)}
 
-                <input type="radio" id="m" name="gender" value="M" onChange={handleChange} checked={values.gender === 'M'} required/>
-                <label for="m">Male</label>
+                        <label for="age">Age</label>
+                        <input type="number" placeholder="Age" name="age" value={values.age} onChange={handleChange} className={errors.age && touched.age ? 'input-error' : null} required/>
+                        {errors.age && touched.age && (<span className="error">{errors.age}</span>)}
+                    </div>
 
-                <input type="radio" id="pnta" name="gender" value="PNTA" onChange={handleChange} required/>
-                <label for="pnta">Prefer Not To Answer</label>
+                    <div className="edit-radio-buttons">
+                        <input type="radio" id="f" name="gender" value="F" onChange={handleChange} checked={values.gender === 'F'} required/>
+                        <label for="f">Female</label>
 
-                <input type="number" placeholder="Age" name="age" value={values.age} onChange={handleChange} className={errors.age && touched.age ? 'input-error' : null} required/>
-                {errors.age && touched.age && (<span className="error">{errors.age}</span>)}
+                        <input type="radio" id="m" name="gender" value="M" onChange={handleChange} checked={values.gender === 'M'} required/>
+                        <label for="m">Male</label>
 
-                <button type="submit">Confirm</button>
-                <button type="button" onClick={onToggleForm}>Cancel</button>
-            </form>
+                        <input type="radio" id="pnta" name="gender" value="PNTA" onChange={handleChange} required/>
+                        <label for="pnta">Prefer Not To Answer</label>
+                    </div>
+                    <button type="submit">Confirm</button>
+                    <button type="button" onClick={onToggleForm}>Cancel</button>
+                </form>
         </div>
+        </>
     )
 
 }
