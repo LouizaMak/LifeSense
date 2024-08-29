5c74c36131a4dc01a4aa4c7fae1acd1718b7eeb9
Louiza Mak <louiza.mak@gmail.com>
2024-08-29 10:24:59 -0500

    style sign up component

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,6 +3,7 @@ import { useFormik } from "formik";
 import { signupSchema } from "../schemas/schemas";
 import { AppContext } from "./AppContext";
 import { useNavigate } from "react-router-dom";
+import style from "./signupStyle.css";
 
 function Signup() {
     const { setCurrentUser } = useContext(AppContext)
@@ -41,40 +42,48 @@ function Signup() {
     })
 
     return(
-        <>
-            <form onSubmit={handleSubmit}>
-                <h2>Signup</h2>
+        <div className="signup-form-container">
+            <form className="signup-form" onSubmit={handleSubmit}>
+                <h2>Sign Up</h2>
 
-                <input type="text" placeholder="Username" name="username" value={values.username} onChange={handleChange} className={errors.username && touched.username ? 'input-error' : null} required/>
-                {errors.username && touched.username && (<span className="error">{errors.username}</span>)}
+                <div className="text-input">
 
-                <input type="text" placeholder="Password" name="password" value={values.password} onChange={handleChange} className={errors.password && touched.password ? 'input-error' : null} required/>
-                {errors.password && touched.password && (<span className="error">{errors.password}</span>)}
+                    <input type="text" placeholder="Username" name="username" value={values.username} onChange={handleChange} className={errors.username && touched.username ? 'input-error' : null} required/>
+                    {errors.username && touched.username && (<span className="error">{errors.username}</span>)}
 
-                <input type="text" placeholder="Email" name="email" value={values.email} onChange={handleChange} className={errors.email && touched.email ? 'input-error' : null} required/>
-                {errors.email && touched.email && (<span className="error">{errors.email}</span>)}
+                    <input type="text" placeholder="Password" name="password" value={values.password} onChange={handleChange} className={errors.password && touched.password ? 'input-error' : null} required/>
+                    {errors.password && touched.password && (<span className="error">{errors.password}</span>)}
 
-                <input type="text" placeholder="First Name" name="first_name" value={values.first_name} onChange={handleChange} className={errors.first_name && touched.first_name ? 'input-error' : null} required/>
-                {errors.first_name && touched.first_name && (<span className="error">{errors.first_name}</span>)}
+                    <input type="text" placeholder="Email" name="email" value={values.email} onChange={handleChange} className={errors.email && touched.email ? 'input-error' : null} required/>
+                    {errors.email && touched.email && (<span className="error">{errors.email}</span>)}
 
-                <input type="text" placeholder="Last Name" name="last_name" value={values.last_name} onChange={handleChange} className={errors.last_name && touched.last_name ? 'input-error' : null} required/>
-                {errors.last_name && touched.last_name && (<span className="error">{errors.last_name}</span>)}
+                    <input type="text" placeholder="First Name" name="first_name" value={values.first_name} onChange={handleChange} className={errors.first_name && touched.first_name ? 'input-error' : null} required/>
+                    {errors.first_name && touched.first_name && (<span className="error">{errors.first_name}</span>)}
 
-                <input type="radio" id="f" name="gender" value="F" onChange={handleChange} checked={values.gender === 'F'} required/>
-                <label for="f">Female</label>
+                    <input type="text" placeholder="Last Name" name="last_name" value={values.last_name} onChange={handleChange} className={errors.last_name && touched.last_name ? 'input-error' : null} required/>
+                    {errors.last_name && touched.last_name && (<span className="error">{errors.last_name}</span>)}
 
-                <input type="radio" id="m" name="gender" value="M" onChange={handleChange} checked={values.gender === 'M'} required/>
-                <label for="m">Male</label>
+                    <input type="number" placeholder="Age" name="age" value={values.age} onChange={handleChange} className={errors.age && touched.age ? 'input-error' : null} required/>
+                    {errors.age && touched.age && (<span className="error">{errors.age}</span>)}
 
-                <input type="radio" id="pnta" name="gender" value="PNTA" onChange={handleChange} required/>
-                <label for="pnta">Prefer Not To Answer</label>
+                </div>
 
-                <input type="number" placeholder="Age" name="age" value={values.age} onChange={handleChange} className={errors.age && touched.age ? 'input-error' : null} required/>
-                {errors.age && touched.age && (<span className="error">{errors.age}</span>)}
+                <div className="gender-radio">
 
-                <button type="submit">Signup</button>
+                    <input type="radio" id="f" name="gender" value="F" onChange={handleChange} checked={values.gender === 'F'} required/>
+                    <label for="f">Female</label>
+
+                    <input type="radio" id="m" name="gender" value="M" onChange={handleChange} checked={values.gender === 'M'} required/>
+                    <label for="m">Male</label>
+
+                    <input type="radio" id="pnta" name="gender" value="PNTA" onChange={handleChange} required/>
+                    <label for="pnta">Prefer Not To Answer</label>
+
+                </div>
+
+                <button type="submit">Sign Up</button>
             </form>
-        </>
+        </div>
     )
 }
 
