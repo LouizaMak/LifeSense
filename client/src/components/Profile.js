b9b96f7cda54e31f138dbd8fb64ed2081129d6ea
Louiza Mak <louiza.mak@gmail.com>
2024-09-04 15:59:23 -0500

    change default image to dynamic avatar

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,8 +1,8 @@
-import { AppContext } from "./AppProvider"
+import { AppContext } from "./AppProvider";
 import React, { useContext, useState } from "react";
 import ProfileForm from "./ProfileForm";
-import profilePicture from "../images/default_picture.jpg";
 import style from "./profileStyle.css"
+import Avatar from '@mui/material/Avatar';
 
 function Profile() {
     const { currentUser } = useContext(AppContext)
@@ -12,12 +12,44 @@ function Profile() {
         setIsEditing(!isEditing)
     }
 
+    function stringToColor(string) {
+        let hash = 0;
+        let i;
+      
+        for (i = 0; i < string.length; i += 1) {
+          hash = string.charCodeAt(i) + ((hash << 5) - hash);
+        }
+      
+        let color = '#';
+      
+        for (i = 0; i < 3; i += 1) {
+          const value = (hash >> (i * 8)) & 0xff;
+          color += `00${value.toString(16)}`.slice(-2);
+        }
+      
+        return color;
+      }
+
+    function stringAvatar(name, additionalSx = {}) {
+        return {
+          sx: {
+            ...additionalSx,
+            bgcolor: stringToColor(name),
+          },
+          children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+        };
+      }
+
+    if (!currentUser) {
+        return <div>Loading...</div>
+    }
+
     return(
         <>
             <div className="profile-block">
                 <div className="profile-intro">
                     <div className="image-button">
-                        <img src={profilePicture} alt="default profile picture"/>
+                        <Avatar {...stringAvatar(`${currentUser.first_name} ${currentUser.last_name}`, { width: 200, height: 200, fontSize: '90px' })} />
                         <button onClick={handleToggleForm}>Edit</button>
                     </div>
                     <div>
